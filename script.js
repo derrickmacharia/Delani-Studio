735a324fd43d23006c8a54c63ee063dd8d8cb65b
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-14 09:58:51 +0300

    made adjustments on the portfolio section

--- script.js
+++ script.js
@@ -4,83 +4,83 @@ $(document).ready(function(){
     $(".design").click(function(){
         $("#design").show();
         $(".design").hide();
-    })
+    });
     $("#design").click(function(){
         $(".design").show();
         $("#design").hide();
-    })
+    });
     $(".development").click(function(){
         $("#development").show();
         $(".development").hide();
-    })
+    });
     $("#development").click(function(){
         $(".development").show();
         $("#development").hide();
-    })
+    });
     $("#product").click(function(){
         $(".product").show();
         $("#product").hide();
-    })
+    });
     $(".product").click(function(){
         $(".product").hide();
         $("#product").show();
-    })
+    });
     // BLANK SPACE1
     $(".custom-container1").mouseenter(function(){
         $(".blank-space1").show()
-    })
+    });
     $(".custom-container1").mouseleave(function(){
         $(".blank-space1").hide()
-    })
+    });
     // BLANK SPACE 2
     $(".custom-container2").mouseenter(function(){
         $(".blank-space2").show()
-    })
+    });
     $(".custom-container2").mouseleave(function(){
         $(".blank-space2").hide()
-    })
+    });
     // BLANK SPACE 3
     $(".custom-container3").mouseenter(function(){
         $(".blank-space3").show()
-    })
+    });
     $(".custom-container3").mouseleave(function(){
         $(".blank-space3").hide()
-    })
+    });
     // BLANK SPACE 4
     $(".custom-container4").mouseenter(function(){
         $(".blank-space4").show()
-    })
+    });
     $(".custom-container4").mouseleave(function(){
         $(".blank-space4").hide()
-    })
+    });
 
 
         // BLANK SPACE 5
     $(".custom-container5").mouseenter(function(){
         $(".blank-space5").show()
-    })
+    });
     $(".custom-container5").mouseleave(function(){
         $(".blank-space5").hide()
-    })
+    });
     // BLANK SPACE 6
     $(".custom-container6").mouseenter(function(){
         $(".blank-space6").show()
-    })
+    });
     $(".custom-container6").mouseleave(function(){
         $(".blank-space6").hide()
-    })
+    });
     // BLANK SPACE 7
     $(".custom-container7").mouseenter(function(){
         $(".blank-space7").show()
-    })
+    });
     $(".custom-container7").mouseleave(function(){
         $(".blank-space7").hide()
-    })
+    });
     // BLANK SPACE 8
     $(".custom-container8").mouseenter(function(){
-        $(".blank-space4").show()
-    })
-    $(".custom-container4").mouseleave(function(){
-        $(".blank-space4").hide()
-    })
+        $(".blank-space8").show()
+    });
+    $(".custom-container8").mouseleave(function(){
+        $(".blank-space8").hide()
+    });
 });
